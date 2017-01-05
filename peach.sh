078742a6f23ede41954870dbe9fe98f9938c5365
Michael Eddington <mike@dejavusecurity.com>
2017-01-05 14:04:21 -0800

    testing

--- peach.sh
+++ peach.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 source venv/bin/activate
+
+cd /opt/sdk/libraries/python
+pip install -r requirements.txt
+python setup.py install
+
+cd /opt/sdk/testrunners/custom/python
+pip install -r requirements.txt
+
+cd /opt/sdk/testrunners/pytest-peach/src
+python setup.py install
+
 python /opt/sdk/ci/generic/peach_ci_runner.py
+
+# end
